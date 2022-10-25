030c051c92b33d10974a9ba98e558467d1f43f86
harryzhu <harry.chuh@gmail.com>
2022-10-25 23:54:42 +0800

    -

--- cmd/http2s.go
+++ cmd/http2s.go
@@ -6,6 +6,7 @@ package cmd
 
 import (
 	"fmt"
+	"sqlconf"
 
 	"github.com/spf13/cobra"
 )
@@ -18,9 +19,19 @@ var http2sCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("http2 server called")
 		//var StaticRootDir string = "/Volumes/HDD4/downloads/"
+		allowiplist := sqlconf.StringToSlice(config.ToString("http2s_allow_ip_list"))
+		blockiplist := sqlconf.StringToSlice(config.ToString("http2s_block_ip_list"))
+		defaultallow := config.ToBool("http2s_default_allow")
 
 		h2s := config.SetH2Server().H2Server
 		h2s.WithStaticRootDir(config.ToString("http2s_static_root_dir"))
+		for _, aip := range allowiplist {
+			h2s.WithAllowIP(aip)
+		}
+		for _, bip := range blockiplist {
+			h2s.WithBlockIP(bip)
+		}
+		h2s.WithDefaultAllow(defaultallow)
 		h2s.WithIP("127.0.0.1")
 		h2s.WithPort(config.ToInt("http2s_port"))
 		h2s.WithTLS(config.ToString("http2s_tls_cert"), config.ToString("http2s_tls_key"))
